52c4805fca7131f4cce74ae7e975680c73ed4304
nesarihan <demirneslihan85@gmail.com>
2023-11-21 19:43:18 +0300

    .

--- ros_esp_mr/esp_src/ultrasonic_interfacing/ultrasonic_interfacing.ino
+++ ros_esp_mr/esp_src/ultrasonic_interfacing/ultrasonic_interfacing.ino
@@ -1,6 +1,6 @@
 
 #include <ros.h>
-#include <std_msgs/Float32.h>
+#include <std_msgs/Int16.h>
 const int trig_pin = 2;
 const int echo_pin = 4;
 
@@ -8,11 +8,11 @@ const int echo_pin = 4;
 #define SPEED_OF_SOUND 0.034
 
 long duration;
-float distance_in_cm;
+int16_t distance_in_cm;
 
 ros::NodeHandle  nh;
 
-std_msgs::Float32 ultrasonic_msg;
+std_msgs::Int16 ultrasonic_msg;
 ros::Publisher ultrasonic_node("ultrasonic_values", &ultrasonic_msg);
 
 void setup(){
@@ -31,7 +31,7 @@ void loop(){
     digitalWrite(trig_pin, LOW);
     
     duration = pulseIn(echo_pin, HIGH);
-    distance_in_cm = (duration * SPEED_OF_SOUND)/2;
+    distance_in_cm = (duration* SPEED_OF_SOUND)/2;
     ultrasonic_node.publish(&ultrasonic_msg);
     nh.spinOnce();
     delay(1);
